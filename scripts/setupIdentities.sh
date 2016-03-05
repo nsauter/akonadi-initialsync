967f28599778cbad12a0694a1b04fe9392126f6a
Christian Mollekopf <chrigi_1@fastmail.fm>
2016-03-05 18:01:47 +0100

    Added an identities migration script.

---
+++ scripts/setupIdentities.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#This script copies an existing identites file, and set's it up with a transport from,
+#an newly configured identities file.
+
+#This should be correct if the migration has just been executed, keep.
+EXISTINGID=~/.kde/share/config/emailidentities
+#Adjust this to the source identities to migrate
+IDENTITIESTOMIGRATE=~/.kde3/share/config/emailidentities
+#This should be correct, keep
+TARGETFILE=~/.kde/share/config/emailidentities
+
+#Extract the transport from the existing identities file
+TRANSPORT=$(grep Transport $EXISTINGID)
+
+#Copy the source identities file (this may overwrite the EXISTINGID file)
+cp $IDENTITIESTOMIGRATE $TARGETFILE
+
+#Setup a valid transport
+sed -i "s/Transport=*/$TRANSPORT/g" $TARGETFILE
+#Remove fields that are no longer valid (references to folders)
+sed -i "/Templates=*/d" $TARGETFILE
+sed -i "/Fcc=*/d" $TARGETFILE
+sed -i "/Drafts=*/d" $TARGETFILE
+sed -i "/Bcc=*/d" $TARGETFILE
+
+echo "Identities migrated with transport: $TRANSPORT"
