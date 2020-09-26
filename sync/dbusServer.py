9b2c1f61edd8193f277d34c88519d7608225d9be
nsauter <sauter.nico99@gmail.com>
2020-09-26 16:46:04 +0000

    fix dbus for python3

--- sync/dbusServer.py
+++ sync/dbusServer.py
@@ -6,46 +6,55 @@
     Date: 2015-12-02
     Licence: GPL-2+
 """
-
-import os, signal
+import os
+import signal
 import subprocess
 
 import logging
 
-logger = logging.getLogger("DBusServer")
+logger = logging.getLogger('DBusServer')
 
-class DBusServer():
-	"""starts/stops a dbus server, if non is running with enter/exit the with statement."""
-	def __init__(self):
-	    self.startOwnServer = False
-            self.environ = {}
 
-        def __enter__(self):
-	    if not self.running():
-               self.start()
-	    return self
+class DBusServer:
 
-        def __exit__(self, type, value, traceback):
-            if self.startOwnServer:
-               self.stop()
+    """starts/stops a dbus server, if non is running with enter/exit the with statement."""
 
-	def running(self):
-	    return os.environ.has_key("DBUS_SESSION_BUS_ADDRESS")
+    def __init__(self):
+        self.startOwnServer = False
+        self.environ = {}
 
-	def start(self):
-	    logger.info("starting dbus...")
-	    p = subprocess.Popen('dbus-launch', shell=True, stdout=subprocess.PIPE)
-	    for var in p.stdout:
-		sp = var.strip().split('=', 1)
-		self.environ[sp[0]] = os.environ.get(sp[0])
-		os.environ[sp[0]] = sp[1]
-            self.startOwnServer = True
+    def __enter__(self):
+        if not self.running():
+            self.start()
+        return self
 
-        def stop(self):
-	    logger.info("stopping dbus...")
-            os.kill(int(os.environ["DBUS_SESSION_BUS_PID"]), signal.SIGTERM)
-            for k in self.environ:
-                if self.environ[k] is None:
-		    del(os.environ[k])
- 		else:
-                    os.environ[k] = self.environ[k]
+    def __exit__(
+        self,
+        type,
+        value,
+        traceback,
+        ):
+        if self.startOwnServer:
+            self.stop()
+
+    def running(self):
+        return 'DBUS_SESSION_BUS_ADDRESS' in os.environ
+
+    def start(self):
+        logger.info('starting dbus...')
+        p = subprocess.Popen('dbus-launch', shell=True,
+                             stdout=subprocess.PIPE)
+        for var in p.stdout:
+            sp = var.strip().split('=', 1)
+            self.environ[sp[0]] = os.environ.get(sp[0])
+            os.environ[sp[0]] = sp[1]
+        self.startOwnServer = True
+
+    def stop(self):
+        logger.info('stopping dbus...')
+        os.kill(int(os.environ['DBUS_SESSION_BUS_PID']), signal.SIGTERM)
+        for k in self.environ:
+            if self.environ[k] is None:
+                del os.environ[k]
+            else:
+                os.environ[k] = self.environ[k]
